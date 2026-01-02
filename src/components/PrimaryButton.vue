4cc5a125aaa5c765bf048217d7d8ff74ce1caca2
takahashiyb <takahashiyb89@gmail.com>
2026-01-02 20:21:59 +0800

    Create Primary Button

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  background-color: hsl(var(--red-color));
+  color: hsl(var(--white-color));
+
+  font: var(--font-preset-3);
+
+  padding: var(--spacing-0200) var(--spacing-0300);
+
+  border-radius: 9em;
+  border: none;
+}
+
+button:hover {
+  background-color: hsl(var(--rose-500-color));
+}
+</style>
